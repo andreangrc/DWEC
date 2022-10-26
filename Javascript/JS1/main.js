6895790ea0da666214a6d070b5870f1e3b2823e6
Andrea Ángel García <andrea.angel@adaits.es>
2022-10-26 20:47:06 +0200

    Actualización

---
+++ Javascript/JS1/main.js
@@ -0,0 +1,27 @@
+
+
+var datos = document.getElementById("tablita");
+
+
+fuction crearTabla() {
+    let filas = parseInt(prompt('Elija el numero de filas'));
+    let columnas = parseInt(prompt('Elija el numero de columnas'));
+}
+tablita.innerHTML = `
+    <tr>       
+        <td>Celda 1</td>
+
+        <td>Celda 2</td>
+
+        <td>Celda 3</td>
+    </tr>
+
+    <tr>
+        <td>Celda 4</td>
+
+        <td>Celda 5</td>
+
+        <td>Celda 6</td>
+    </tr>
+    `;
+
