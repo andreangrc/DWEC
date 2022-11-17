4f57a0181f88f3728d0b267e711a8d8d4f1bebc3
Andrea Ángel García <andrea.angel@adaits.es>
2022-11-17 10:25:10 +0100

    Actualización final protractor

--- Javascript/JS2/pruebasProtactor/prueba1.js
+++ Javascript/JS2/pruebasProtactor/prueba1.js
@@ -1,10 +1,11 @@
 // spec.js
-describe('Protractor Demo App', function () {
+describe('Conjunto de pruebas para comprobar la funcionalidad del boletín JS2', function () {
   it('Prueba 1: Berlín', function () {
     browser.waitForAngularEnabled(false);
     //PASO 1 ->
     //ACCIÓN: Abrir la URL
     browser.get('file:///B:/Usuarios/ADA%20ITS%202021-2023/SEGUNDO%20CURSO/DWEC/Javascript/JS2/index.html');
+    browser.sleep(3000);
     //CONDICIÓN: Se muestra una lista de paises y capitales
     var filas = element.all(by.xpath('//tr'));
     expect(filas.count()).toEqual(21);
@@ -60,10 +61,10 @@ describe('Protractor Demo App', function () {
     //CONDICIÓN: Se muestra la tabla con esa capital y país únicamente
     var capital = element.all(by.xpath('//tr[@id="visible"]'));
     expect(capital.count()).toEqual(0);
-    browser.sleep(3000);
+    browser.sleep(5000);
   });
       
-  it('Prueba 1: Alemania', function () {
+  it('Prueba 4: Alemania', function () {
     //PASO 1 ->
     //ACCIÓN: Abrir la URL
     browser.get('file:///B:/Usuarios/ADA%20ITS%202021-2023/SEGUNDO%20CURSO/DWEC/Javascript/JS2/index.html');
@@ -81,6 +82,6 @@ describe('Protractor Demo App', function () {
     //CONDICIÓN: Se muestra la tabla con esa capital y país únicamente
     var capital = element.all(by.xpath('//tr[@id="visible"]'));
     expect(capital.count()).toEqual(0);
-    browser.sleep(3000);
+    browser.sleep(5000);
   });
 });
