6552547344ca338bbf85eabda9201cb4e5fbdde5
Andrea Ángel García <andrea.angel@adaits.es>
2023-01-13 17:57:12 +0100

    Actualización clase
    Login acabado, falta retocar varias cosas

--- Angular/A1/angularUno/src/app/app.module.ts
+++ Angular/A1/angularUno/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login.component';
+import { RegisterComponent } from './components/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/A1/angularUno/src/app/components/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  constructor(){
+    console.log("Componente login");
+  }
+}
