6f48b74874d2b40fa989b6ede602eaa80c783bde
Andrea Ángel García <andrea.angel@adaits.es>
2023-01-20 18:07:38 +0100

    Actualización clase

--- Angular/A1/angularUno/src/app/app.module.ts
+++ Angular/A1/angularUno/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login.component';
-import { RegisterComponent } from './components/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
