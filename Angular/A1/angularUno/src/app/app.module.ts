b5439ebd6a46829b8801649d872f989c92dd3540
Andrea Ángel García <andrea.angel@adaits.es>
2023-01-24 15:20:18 +0100

    Creación de componentes de registro y footer

--- Angular/A1/angularUno/src/app/app.module.ts
+++ Angular/A1/angularUno/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login.component';
+import { RegisterComponent } from './components/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/A1/angularUno/src/app/components/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+
+export class RegisterComponent {
+  constructor(){
+    console.log("Componente registrarse");
+  }
+}
