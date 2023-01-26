2795538a3c6e6c805413b006c39fc94f4a6d46c4
Andrea Ángel García <andrea.angel@adaits.es>
2023-01-26 14:25:25 +0100

    Actualización casa

--- Angular/A1/angularUno/src/app/app.module.ts
+++ Angular/A1/angularUno/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login.component';
-import { RegisterComponent } from './components/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegistroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
